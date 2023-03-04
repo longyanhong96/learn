4fdb5e5776ea326e9517ddc68217da6504a9097e
longyh <498141896@qq.com>
2023-03-04 19:10:49 +0800

    add day1数组，集合的练习题

---
+++ src/main/java/day1/Solution.java
@@ -0,0 +1,56 @@
+package day1;
+
+import java.util.List;
+
+/**
+ * @author longyh
+ * @Description:
+ * @analysis:
+ * @date 2023/3/4 18:50
+ */
+public class Solution {
+
+    // 对一维数组排序
+    public void soft(int[] arr) {
+
+    }
+
+    // 字符串反转 'abcdefg' 反转成 'gfedcba'
+    public String reverse(String str) {
+        return null;
+    }
+
+
+    // 统计大写字母个数 'aAshdiuYUUO'
+    public int countNum(String str) {
+        return 0;
+    }
+
+    // 打印出所有的"水仙花数"，所谓"水仙花数"是指一个三位数，其各位数字立方和等于该数本身。例如：153是一个"水仙花数"，因为153=1的三次方＋5的三次方＋3的三次方
+    public List<Integer> getDaffodils() {
+        return null;
+    }
+
+    // 求阶乘和 求1！+2！+3！+4！+…+n!的和
+    public int getSum(int n) {
+        return 0;
+    }
+
+    /**
+     * 定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 target 的那 两个 整数，并返回它们的数组下标;
+     * 输入：nums = [2,7,11,15], target = 9
+     * 输出：[0,1]
+     */
+    public int[] twoSum(int[] nums, int target) {
+        return null;
+    }
+
+    /**
+     * 给你一个 非空 整数数组 nums ，除了某个元素只出现一次以外，其余每个元素均出现两次。找出那个只出现了一次的元素。
+     * 输入：nums = [2,2,1]
+     * 输出：1
+     */
+    public int singleNumber(int[] nums) {
+        return 0;
+    }
+}
