f2f47cb1ef829512a376b80232fb53ff0dfa5e74
longyh <498141896@qq.com>
2023-03-07 10:48:14 +0800

    add 36~42 面对对象介绍     71～84 数据和字符串

--- src/main/java/test2.java
+++
@@ -1,9 +0,0 @@
-public class test2 {
-    public static void main(String[] args) {
-        String name;
-        name="zhangsan";
-        System.out.println(name);
-        String username="lisi";
-        System.out.println(username);
-    }
-}
